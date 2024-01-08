d3832f01409aad55fb083f74fcb3e294a9e76ed7
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-01-08 12:43:00 -0600

    Changes made extracting common functionality to reusable methods. Not completed.

--- passwd_mgr.rb
+++ passwd_mgr.rb
@@ -12,53 +12,98 @@ As someone who has worked with and is involved in CyberSec... I feel triggered.
 =end
 
 
-puts "Generic - Totally Seucure (sic!) - Passowrd Manager"
-puts "Please sign-in to continue."
-print "Enter UserId (email or ID):"
-user_id = gets.chomp
-
-if user_id == USER
-  print "Enter Password: "
-  password = gets.chomp
-else
-  puts "Invalid or Unknown User."
-  exit
+def welcome_message
+  puts "Generic - Totally Seucure (sic!) - Passowrd Manager"
 end
 
-unless password != PASSWORD
+def prompt_user_for_email
+  puts "Please sign-in to continue."
+  print "Enter UserId (email or ID):"
+  gets.chomp
+end
+
+def verify_user_email
+  if user_id != USER
+    puts "Invalid or Unknown User."
+    exit
+  end
+end
+
+def prompt_user_for_password
+  print "Please enter your Password: "
+  gets.chomp
+end
+
+def verify_user_password(user_password)
+  if user_password != PASSWORD
+    puts "Invalid password."
+    exit
+  end
+end
+
+def greet_user(user_email)
   puts "Welcome, ${user_id}."
+end
+
+def menu_options
   puts "Choose an option:"
   puts "1. Add new service credentials."
   puts "2. Retrieved existing credentials."
   puts "3. Exit"
-  selection = gets.chomp
-  puts "Select was #{selection}"
-else
-  puts "Invalid password."
-  exit
 end
 
-case selection
-when "1"
+def get_user_menu_selection
+  gets.chomp
+end
+
+def handle_user_menu_selection(user_selection)
+  case selection
+  when "1"
+    new_service = set_new_service_name
+    set_username_for(new_service)
+    set_password_for_service(new_service)
+  when "2"
+    print "What is the service name you wish to retrieve? "
+    selected_service = get_service_name
+    credentials = get_service_credentials
+    display_service_credentials(credentials, selected_service)
+
+  else
+    puts "Exiting the program."
+    exit
+  end
+end
+
+def set_new_service_name
   print "Enter the nane of the service:"
   new_service = gets.chomp
-  PASSWORD_VAULT[new_service] = {}
+  PASSWORD_VAULT[new_service.to_sym] = {}
+  new_service
+end
+
+def set_username_for(service)
   print "Enter the username for the #{new_service} service:"
   new_service_username = gets.chomp
-  PASSWORD_VAULT[new_service][:username] = new_service_username
-  print "Enter the password for the #{new_service} service:"
-  PASSWORD_VAULT[new_service][:password] = gets.chomp
-  p PASSWORD_VAULT
-when "2"
-  print "What is the service name you wish to retrieve? "
-  selected_service = gets.chomp
-  credentials = PASSWORD_VAULT[selected_service.to_sym]
+  PASSWORD_VAULT[service.to_sym][:username] = new_service_username
+end
 
+def set_password_for_service(service)
+  print "Enter the password for the #{new_service} service:"
+  PASSWORD_VAULT[service.to_sym][:password] = gets.chomp
+end
+
+def get_service_name
+  print "What is the name of the service you'd like info on? "
+  gets.chomp
+end
+
+def get_service_credentials(service_name)
+  PASSWORD_VAULT[service_name.to_sym]
+end
+
+def display_service_credentials(credentials, service_name)
   puts "Here are the credentials for #{selected_service}:"
-  credentials.each do |k, v|
-    puts "#{k} #{v}"
-  end
-else
-  puts "Exiting the program."
-  exit
+    credentials.each do |k, v|
+      puts "#{k}: #{v}"
+    end
 end
